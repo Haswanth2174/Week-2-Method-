cb55d70bf3463d2c4eaf6d9beb864fcdbd93108a
Haswanth Reddy Mekala <Mekala-HR@ulster.ac.uk>
2024-10-01 11:57:23 +0100

    Week-2 (Methods ) is done

--- Week-2 (Methods)/Program.cs
+++ Week-2 (Methods)/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+Main();
+void Main()
+{
+    try
+    {
+    // Prompting the user to input the length of the rectangle
+    Console.WriteLine("Please enter the length of the Rectangle");
+    double l = Convert.ToDouble(Console.ReadLine());
+    // Prompting the user to input the breadth of the rectangle
+    Console.WriteLine("Please enter the breadth of the Rectangle");
+    double b = Convert.ToDouble(Console.ReadLine());
+    //creted a method to calculate the area of the rectangle
+    double area = CalculateArea(l, b);
+    Console.WriteLine($"The area of the rectangle is {area}");// displaying the area of the rectangle
+    }
+    catch (Exception ex)
+    {// handling the exception errors
+        Console.WriteLine($"The application has the following error.{ex.Message}.Please restart the application...");
+    }
+}
+
+static double CalculateArea(double l, double b)
+{
+    double area = l*b;// formula for calculating the area of the rectangle
+    return area;
+}
